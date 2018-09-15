9fc244984fde22dc29d9f4210ac7138955227f7e
yongjixl <18770092112@163.com>
2018-09-15 12:28:22 +0800

    Initial commit

---
+++ src/main/java/com/laboratory/management/system/laboratorymanagementsystem/LaboratoryManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.laboratory.management.system.laboratorymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratoryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratoryManagementSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/laboratory/management/system/laboratorymanagementsystem/LaboratoryManagementSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.laboratory.management.system.laboratorymanagementsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LaboratoryManagementSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
